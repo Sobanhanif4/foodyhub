c1af26404ef9c03bfb0930c574710268871dea54
Soban hanif <sobanatban4@gmail.com>
2024-08-01 06:19:41 +0500

    2nd commit

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -9,11 +9,11 @@ const GlobalStyle = createGlobalStyle`
   *{
     box-sizing: border-box;
     margin: 0;
-    padding:0;
+    padding: 0;
   }
   body{
-    background-color: #323334;
-    color:white;
+    background-color:  #323334;
+    color: white;
     min-height: 100vh;
     font-family:  'Inter', sans-serif;
   }
