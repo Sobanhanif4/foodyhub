df1bd66fbce0d6717831b7a3f260f0aa0c839447
Soban hanif <sobanatban4@gmail.com>
2024-08-02 21:40:12 +0500

    completed

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -2,13 +2,14 @@ import { useEffect, useState } from 'react';
 import styled from 'styled-components'
 import SearchResult from './components/SearchResults/SearchResult';
 
-const BASE_URL = 'http://localhost:9000/'
+export const BASE_URL = 'http://localhost:9000'
 
 const App = () => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState(null)
-
+  const [filteredData, setFilteredData] = useState(null)
+  const [selectedBtn, setselectedBtn] = useState('all');
 
 
 
@@ -16,15 +17,16 @@ const App = () => {
 
     const fetchFoodData = async () => {
       setLoading(true);
-      
+
       try {
         const response = await fetch(BASE_URL)
-        
-        console.log(typeof(response));
+
+        console.log(typeof (response));
         const json = await response.json()
         // typeof(json);
         // console.log(typeof(json));
         setData(json);
+        setFilteredData(json);
         setLoading(false)
       } catch (error) {
         setError("Unable to fetch data");
@@ -32,57 +34,94 @@ const App = () => {
     };
     fetchFoodData();
   }, []);
- console.log(data);
+  console.log(data);
   // fetchFoodData();
 
-// const temp = [
-//     {
-//       "name": "Boilded Egg",
-//       "price": 10,
-//       "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
-//       "image": "/images/egg.png",
-//       "type": "breakfast"
-//   }
-// ];
+  const searchFood = (e) => {
+    const searchValue = e.target.value;
+
+    if (searchValue === " ") {
+      setFilteredData(null)
+    }
+    const filter = data?.filter((food) => food.name.toLowerCase().includes(searchValue.toLowerCase()));
+
+    setFilteredData(filter)
+  };
+
+  const filterFood = (type) => {
+    if (type === "all") {
+      setFilteredData(data);
+      setselectedBtn("all");
+      return;
+    }
+    const filter = data?.filter((food) => food.type.toLowerCase().includes(type.toLowerCase()));
+    setFilteredData(filter);
+    setselectedBtn(type);
+  };
+
+  const filterBtn = [
+    {
+      name: "All",
+      type: "all",
+    },
+    {
+      name: "Breakfast",
+      type: "breakfast",
+    },
+    {
+      name: "Lunch",
+      type: "lunch",
+    },
+    {
+      name: "Dinner",
+      type: "dinner",
+    },
+  ];
 
   if (error) return <div>{error}</div>
   if (loading) return <div>loading....</div>
 
   return (
-    <Container>
+    <><Container>
       <TopContainer>
         <div className='logo'>
           <img src="/logo.svg" alt="logo" />
         </div>
 
         <div className='search'>
-          <input
+          <input onChange={searchFood}
             placeholder='Search Food' />
         </div>
       </TopContainer>
 
       <FilterContainer>
-        <Button>All</Button>
-        <Button>Breakfast</Button>
-        <Button>Lunch</Button>
-        <Button>Dinner</Button>
+        {
+          filterBtn.map((value) => (
+            <Button
+            
+            isSeclected = {selectedBtn === value.type}
+            key={value.name} onClick={() => filterFood (value.type)} >{value.name}</Button>
+          ) )
+        }
+
       </FilterContainer>
 
-      <SearchResult data= { data } />
     </Container>
+      <SearchResult data={filteredData} />
+    </>
   );
 };
 
 export default App
 
-const Container = styled.div`
+export const Container = styled.div`
 max-width: 1200px;
 margin: 0 auto;
 
 `;
 
 const TopContainer = styled.section`
-min-height: 140px;
+height: 140px;
 display: flex;
 justify-content: space-between;
 padding: 16px;
@@ -97,8 +136,13 @@ input{
   height: 40px;
   font-size:16px;
   padding: 0 10px;
+  &:: placeholder{
+  color: white;
+  }
 }
 }
+
+
 `;
 
 const FilterContainer = styled.section`
@@ -109,10 +153,15 @@ padding-bottom: 48px
 `;
 
 
-const Button = styled.button`
-background: #FF4343;
+export const Button = styled.button`
+background: ${({isSeclected}) => (isSeclected) ? "#f22f2f" : "#FF4343"};
+outline: 1px solid ${({isSeclected}) => (isSeclected) ? "white" : "#FF4343"};
 border-radius: 5px;
 padding: 6px 12px 6px 12px;
 border: none;
+cursor: pointer;
+&:hover{
+background-color: #f22f2f;
+}
 `;
 
